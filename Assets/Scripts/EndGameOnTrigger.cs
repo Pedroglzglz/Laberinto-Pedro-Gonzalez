5b73bb22e5d31c3562e54970dc2ece3e3cef598e
Pedroglzglz <pegonzalez998@gmail.com>
2024-12-09 20:55:41 +0100

    Cambios
    Añadidos elementos de guía y fin del juego.

---
+++ Assets/Scripts/EndGameOnTrigger.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndGameOnTrigger : MonoBehaviour
+{
+
+    public GameObject gameOverPanel;
+    public GameObject gameOverPanel2;
+    // Start is called before the first frame update
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            
+
+            gameOverPanel.SetActive(true);
+            gameOverPanel2.SetActive(true);
+            Time.timeScale = 0f;
+           
+        }
+    }
+}
---
+++ Assets/Scripts/PlaySoundOnTrigger.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySoundOnTrigger : MonoBehaviour
+{
+
+    public AudioSource audioSource;
+    // Start is called before the first frame update
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            audioSource.Play();
+
+        }
+    }
+}
